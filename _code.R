d41df959b3347f3ed75ea7f14fdcf3c289ebb99c
cmlrqn <forteg100@gmail.com>
2019-06-29 19:11:44 +0100

    code revamped; data uploaded. next step: obtain information for each single record

--- _code.R
+++ _code.R
@@ -1,179 +1,136 @@
-library('ggplot2')
-library('rvest')
-library('tidyverse')
+setwd("/Users/gforte/Dropbox/git/metacritic")
 
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load('ggplot2','rvest','tidyverse','foreach','doParallel','lubridate')
 
-#Storage data.frame
-df_mc = data.frame()
-
-#### Loop over Artist By Name pages ####
-
-
-webpages <- c(apply(expand.grid("http://www.metacritic.com/browse/albums/artist", 
-              c("", paste0("/", letters )), "?view=condensed&page", 1:24), 1, paste, collapse=""))
-webpages <- gsub(" ", "", webpages)
-
-# url_check is used to check we are not re-scraping the same page
-url_check <- ""
-
-for (page in 1:length(webpages)) {
-
-  url <- read_html(webpages[page])
-  if (identical(url, url_check)) next
+# Function to read the webpage
+pagetotable <- function(list, index) {
+  cat("page", index, "\n" )
+  url <- read_html(list[index])
   
   table <- html_nodes(url, ".release_product") %>% html_text() %>% unlist()
   table <- gsub("\n\n\n\n\n\n\n", "", table)
   table <- gsub("\\s+", " ", table) %>% trimws()
   
   table_chop <- strsplit(table, "Release Date: |User: |Artist: ")
-  table_chop <- table_chop %>% as.data.frame() %>% t() %>% data.frame()
-  table_chop$rating <- ""
-  q <- nchar(as.character(table_chop$X1))
-  table_chop$rating <- substr(table_chop$X1,  q - 3, q) %>% trimws()
-  table_chop$X1 <- table_chop$X1 %>% substr( 1, q - 4) %>% trimws()
-  rownames(table_chop) <- NULL
-  clnms <- c("Title", "Artist", "User Score", "Release Date", "Metascore")
-  colnames(table_chop) <- clnms
+  table_chop <- data.frame(matrix(unlist(table_chop), nrow=length(table_chop), byrow=T))
+  table_chop[,1] <- as.character(table_chop[,1])
+  table_chop[,5] <- substr(table_chop[, 1], nchar(table_chop[,1])-2, nchar(table_chop[,1]))
+  table_chop[,1] <- substr(table_chop[, 1], 1, nchar(table_chop[,1])-3)
   
-  url_check <- url
-  
-  df_mc <- rbind(df_mc, table_chop)
-  df_mc <- unique(df_mc)
+  return(table_chop)
 }
 
-#Formatting date
-df_mc$date <- tolower(df_mc$`Release Date`)
-df_mc$date <- gsub(",", "", df_mc$date) 
-df_mc$date <- gsub(" ", "", df_mc$date)
-df_mc$date[nchar(df_mc$date)==8] <- paste0(substr(df_mc$date[nchar(df_mc$date)==8],1,3), 0,
-                                          substr(df_mc$date[nchar(df_mc$date)==8],4,nchar(df_mc$date[nchar(df_mc$date)==8])))
-df_mc$`Release Date` <- as.Date(df_mc$date, "%b%d%Y")
-df_mc$date <- NULL
+# Opening the doParallel session 
+coresno <- 3
+cl <- makeCluster(coresno, outfile="")
 
-save(df_mc, file = "metacritic_music.RData")
+#### Loop over album releases by score  ####
+# Only albums with seven or more reviews are eligible. 
+# EPs, live albums, box sets, re-issues, and compilations are also excluded.
 
+webpages <- paste0("https://www.metacritic.com/browse/albums/score/metascore/all/filtered?sort=desc&page=", 0:118)
 
-#### Loop over All Album Releases pages  ####
+registerDoParallel(cl)
 
-#Storage data.frame
-df_mc_alltime = data.frame()
-
-webpages <- c(paste0("http://www.metacritic.com/browse/albums/release-date/available/metascore?page=", 0:77))
-
-# url_check is used to check we are not re-scraping the same page
-url_check <- ""
-
-for (page in 1:length(webpages)) {
+list_mc_alltime <- foreach (page = 1:length(webpages), .packages = "rvest", .combine = list, 
+                            .errorhandling='pass', .multicombine = T, .maxcombine = 500) %dopar% {
   
-  url <- read_html(webpages[page])
-  if (identical(url, url_check)) next
-  
-  table <- html_nodes(url, ".release_product") %>% html_text() %>% unlist()
-  table <- gsub("\n\n\n\n\n\n\n", "", table)
-  table <- gsub("\\s+", " ", table) %>% trimws()
-  
-  table_chop <- strsplit(table, "Release Date: |User: |Artist: ")
-  table_chop <- table_chop %>% as.data.frame() %>% t() %>% data.frame()
-  table_chop$rating <- ""
-  q <- nchar(as.character(table_chop$X1))
-  table_chop$rating <- substr(table_chop$X1,  q - 3, q) %>% trimws()
-  table_chop$X1 <- table_chop$X1 %>% substr( 1, q - 4) %>% trimws()
-  rownames(table_chop) <- NULL
-  clnms <- c("Title", "Artist", "User Score", "Release Date", "Metascore")
-  colnames(table_chop) <- clnms
-  
-  url_check <- url
-  
-  df_mc_alltime <- rbind(df_mc_alltime, table_chop)
-  df_mc_alltime <- unique(df_mc_alltime)
-  
-  Sys.sleep(1)
-}
+    table_chop <- pagetotable(list = webpages, index = page)
+    
+    return(table_chop)
+    
+    Sys.sleep(1)
+  }
 
-#Formatting date
-df_mc_alltime$date <- tolower(df_mc_alltime$`Release Date`)
-df_mc_alltime$date <- gsub(",", "", df_mc_alltime$date) 
-df_mc_alltime$date <- gsub(" ", "", df_mc_alltime$date)
-df_mc_alltime$date[nchar(df_mc_alltime$date)==8] <- paste0(substr(df_mc_alltime$date[nchar(df_mc_alltime$date)==8],1,3), 0,
-                                                           substr(df_mc_alltime$date[nchar(df_mc_alltime$date)==8],4,nchar(df_mc_alltime$date[nchar(df_mc_alltime$date)==8])))
-df_mc_alltime$`Release Date` <- as.Date(df_mc_alltime$date, "%b%d%Y")
-df_mc_alltime$date <- NULL
+stopCluster(cl)
 
-df_mc_alltime <- df_mc_alltime[order(as.character(df_mc_alltime$Artist), df_mc_alltime$`Release Date`),]
-rownames(df_mc_alltime) <- NULL
+# refetch error pages iteratively
+while (length(unique(sapply(list_mc_alltime, length))) != 1) {
+    
+    toredo <- which(sapply(list_mc_alltime, length)==2)
+    cat("there are", length(toredo), "errors: pages", toredo, "\n")
+    for (page in toredo) {
+      
+      table_chop <- pagetotable(list = webpages, index = page)
+      
+      list_mc_alltime[[page]] <- table_chop 
+      }
+  }
 
-save(df_mc_alltime, file = "metacritic_alltime.RData")
+df_mc <- do.call("rbind",list_mc_alltime)
+colnames(df_mc) <- c("title", "artist", "userscore", "release", "metascore")
+df_mc$release <- as.Date(tolower(df_mc$release), "%b %d, %Y")
+df_mc$metascore <- as.numeric(df_mc$metascore)
+rm(list_mc, list_mc_alltime)
 
+save(df_mc, file = "metacritic_albums.RData")
+
+# load("metacritic_albums.RData")
 
 #### Visualisations ####
 
-
 #Analysing trends by month and year
-df_mc_alltime$Month_Yr <- as.Date(df_mc_alltime$`Release Date`, "%Y-%m")
-df_mc_alltime$Yr <- format(df_mc_alltime$`Release Date` , "%Y")
-df_mc_alltime$Metascore <- as.numeric(df_mc_alltime$Metascore)
+df_mc$monthyear <- format(as.Date(df_mc$release, "%Y-%m-%d"), "%Y-%m")
+df_mc$year <- format(df_mc$release, "%Y")
 # There are only ~20 articles from 1999, too small a sample.
-df_mc_cut <- df_mc_alltime[df_mc_alltime$Yr>1999,]
+df_mc_old <- df_mc
+df_mc <- df_mc[df_mc$year>1999,]
 
 #-->> Monthly ----
-month1 <- df_mc_cut %>% mutate(month1 = as.Date(cut(Month_Yr, breaks = "1 month"))) %>%
-  group_by(month1) %>% 
-  summarise(average = mean(Metascore), count = n_distinct(Metascore))
 
-monthlycritic <- ggplot(show.legend= F) +
-                   geom_line(data=month1, aes(x=month1, y=average)) + 
-                   xlab('Month of Release') + ylab('Average Metascore') + 
-                   scale_x_date(date_labels =  "%b %y", date_minor_breaks = "1 month", expand=c(0,0)) +
-                   scale_y_continuous(breaks=c(seq(60, 80, by=1))) +
-                   theme(plot.title = element_text(lineheight=.8, face="bold", hjust = .5)) + 
-                   ggtitle("Average Album Metascore by Month")
+monthly <- df_mc %>% group_by(month=floor_date(release, "month")) %>% 
+           summarise(average = mean(metascore), count = n_distinct(metascore),
+                     p25 = quantile(metascore, probs = .25), 
+                     p50 = quantile(metascore, probs = .5),
+                     p75 = quantile(metascore, probs = .75))
 
+monthlycritic <- ggplot(data = monthly, show.legend= F, aes(month)) + theme_bw() +
+                   geom_line(data=monthly, aes(x=month, y=average)) + 
+                   geom_ribbon(data=monthly,aes(ymin=p25,ymax=p75),alpha=0.3) +
+                   xlab('Month of Release') + ylab('Metascore') +
+                   scale_x_date(date_labels="%b %y", date_minor_breaks="1 month", expand=c(0,0)) +
+                   scale_y_continuous(breaks=c(seq(50, 90, by=2))) +
+                   theme(plot.title = element_text(lineheight=.8, face="bold", hjust=.5)) +
+                   labs(title = "Album Metascores, by Month", 
+                        caption = "note: averages in black; first and third quartile in grey.")
 print(monthlycritic)
 
 
 #-->> Quarterly----
-month3 <- df_mc_cut %>% mutate(month3 = as.Date(cut(Month_Yr, breaks = "3 month"))) %>%
-  group_by(month3) %>% 
-  summarise(average = mean(Metascore), count = n_distinct(Metascore))
 
-quarterlycritic <- (ggplot(data=month3, aes(x=month3, y=average, width=.7)) +
-                      geom_line() + 
-                      xlab('Quarter of Release') + ylab('Average Metascore')) + 
-  scale_x_date(date_labels =  "%b %y", date_minor_breaks = "3 month", expand=c(0,0)) +
-  scale_y_continuous(breaks=c(seq(70,80,by=1))) +
-  theme(plot.title = element_text(lineheight=.8, face="bold", hjust = .5)) + 
-  ggtitle("Average Album Metascore by Quarter")
+quarterly <- df_mc %>% group_by(quarter=floor_date(release, "3 month")) %>% 
+             summarise(average = mean(metascore), count = n_distinct(metascore),
+                        p25 = quantile(metascore, probs = .25), 
+                        p50 = quantile(metascore, probs = .5),
+                        p75 = quantile(metascore, probs = .75))
 
+quarterlycritic <- ggplot(data = quarterly, show.legend= F, aes(quarter)) + theme_bw() +
+                   geom_line(data=quarterly, aes(x=quarter, y=average)) + 
+                   geom_ribbon(data=quarterly,aes(ymin=p25,ymax=p75),alpha=0.3) +
+                   xlab('Quarter of Release') + ylab('Metascore') +
+                   scale_x_date(date_labels="%b %y", date_minor_breaks="3 month", expand=c(0,0)) +
+                   scale_y_continuous(breaks=c(seq(50, 90, by=2))) +
+                   theme(plot.title = element_text(lineheight=.8, face="bold", hjust=.5)) +
+                   labs(title = "Album Metascores, by Quarter", 
+                        caption = "note: averages in black; first and third quartile in grey.")
 print(quarterlycritic)
 
-
-#-->> Quarterly with Albums Scored----
-month3 <- df_mc_cut %>% mutate(month3 = as.Date(cut(Month_Yr, breaks = "3 month"))) %>%
-  group_by(month3) %>% 
-  summarise(average = mean(Metascore), count = n_distinct(Metascore))
-
-quarterlyalbcritic <- (ggplot(data=month3, aes(x=month3, y=average, width=.7)) +
-                       geom_line(color="red") + geom_step(data=month3, aes(x=month3, y=count)) +
-                       xlab('Quarter of Release') + ylab('Average Metascore')) + 
-  scale_x_date(date_labels =  "%b %y", date_minor_breaks = "3 month", expand=c(0,0)) +
-  scale_y_continuous(breaks=c(seq(0,100,by=5)), sec.axis = sec_axis(~., name = "Albums Scored")) +
-  theme(plot.title = element_text(lineheight=.8, face="bold", hjust = .5), legend.position = c(0.8, 0.9)) + 
-  ggtitle("Average Album Metascore by Quarter")
-
-print(quarterlyalbcritic)
-
-
 #-->> Yearly ----
-year1 <- df_mc_cut %>% mutate(year1 = as.Date(cut(Month_Yr, breaks = "1 year"))) %>%
-  group_by(year1) %>% 
-  summarise(average = mean(Metascore), count = n_distinct(Metascore))
 
-yearlycritic <- (ggplot(data=year1, aes(x=year1, y=average), show.legend= F) +
-                       geom_line() +
-                       xlab('Year of Release') + ylab('Average Metascore')) + 
-                       scale_x_date(date_minor_breaks = "1 year") + scale_y_continuous(breaks=c(seq(70, 80,by=1))) +
-                       theme(plot.title = element_text(lineheight=.8, face="bold", hjust = .5)) + 
-                       ggtitle("Average Album Metascore by Year")
+yearly <- df_mc %>% group_by(year=floor_date(release, "12 month")) %>% 
+          summarise(average = mean(metascore), count = n_distinct(metascore),
+                    p25 = quantile(metascore, probs = .25), 
+                    p50 = quantile(metascore, probs = .5),
+                    p75 = quantile(metascore, probs = .75))
 
+yearlycritic <- ggplot(data = yearly, show.legend= F, aes(year)) + theme_bw() +
+                geom_line(data=yearly, aes(x=year, y=average)) + 
+                geom_ribbon(data=yearly,aes(ymin=p25,ymax=p75),alpha=0.3) +
+                xlab('Year of Release') + ylab('Metascore') +
+                scale_x_date(date_labels="%b %y", date_minor_breaks="12 month", expand=c(0,0)) +
+                scale_y_continuous(breaks=c(seq(50, 90, by=2))) +
+                theme(plot.title = element_text(lineheight=.8, face="bold", hjust=.5)) +
+                labs(title = "Album Metascores, by Year", 
+                     caption = "note: averages in black; first and third quartile in grey.")
 print(yearlycritic)
-
